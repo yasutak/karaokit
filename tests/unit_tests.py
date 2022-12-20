1f66f56bf54d34ebce642b8b2ea37d56d63ddd6e
Yasutaka Odo <YasutakaOdo@gmail.com>
2022-12-21 08:29:01 +0900

    remove block quotation

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -11,12 +11,14 @@ from karaokit import karaokit
 ja_song_mp3_path = pathlib.Path("../examples/ja_song.mp3").resolve()
 ja_song_ass_path = pathlib.Path("../examples/ja_song.ass").resolve()
 output_ja_song_ass_path = pathlib.Path("../examples/output_ja_song.ass")
-output_ja_song_mp4_path = pathlib.Path("../examples/output_ja_song.mp4").resolve()
+output_ja_song_mp4_path = pathlib.Path(
+    "../examples/output_ja_song.mp4"
+).resolve()
 
-"""
+
 class TestGenerateSubtitleFileFromAudio(unittest.TestCase):
     def test_generate_subtitle_from_audio(self):
-        generate_subtitle_file_from_audio(
+        generate_subtitle_from_audio(
             mp3_file=ja_song_mp3_path,
             output_file=output_ja_song_ass_path.__str__(),
             language="ja",
@@ -33,8 +35,6 @@ class TestGenerateSubbedVideos(unittest.TestCase):
             resolution="md",
             dry_run=True,
         )
-
-"""
 
 
 class TestKaraokit(unittest.TestCase):
