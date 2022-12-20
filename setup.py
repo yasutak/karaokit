bf1ffdc29bae80bba0100399a771c942edd08a9c
Yasutaka Odo <YasutakaOdo@gmail.com>
2022-12-21 06:11:13 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='karaokit',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A command line tool for generating karaoke-style-subbed-videos from audio',
+    author='Yasutaka Odo',
+    license='MIT',
+)
